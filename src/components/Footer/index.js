817c0888128d81026f90b11533fe391781b1b19e
juuhgouvea <juliasantos752@yahoo.com.br>
2020-07-28 20:51:16 -0300

    setup pages

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import { FooterBase } from './styles';
+import React from "react";
+import { FooterBase } from "./styles";
+import LogoMain from "../../assets/img/LogoMain.png";
 
 function Footer() {
   return (
     <FooterBase>
       <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+        <img className="LogoMain" src={LogoMain} alt="AluraFlix logo" />
       </a>
       <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
+        Orgulhosamente criado durante a{" "}
+        <a href="https://www.alura.com.br/">Imersão React da Alura</a>
       </p>
     </FooterBase>
   );
